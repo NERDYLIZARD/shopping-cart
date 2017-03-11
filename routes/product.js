8d4436500166af8bfcfea5ae68aa525f93e65e5e
NERDYLIZARD <bouasavanhhop@gmail.com>
2017-03-11 15:50:16 +0530

    CRUD: Delete & route guard

--- routes/product.js
+++ routes/product.js
@@ -5,39 +5,30 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var passport = require('passport');
 
 var Product = require('../models/product');
 
 
 router.get('/:id/image', function (req, res, next) {
-
   Product.findById(req.params.id, function (err, product) {
     res.contentType(product.image.contentType);
     res.send(product.image.data);
   });
 });
 
+
+// Route guard
+router.use('/', isLoggedIn, function (req, res, next) {
+  next();
+})
+
+// Add product
 router.get('/form', function (req, res, next) {
   res.render('product/product-form', {
     heading: "Add Product",
     url: "/product/add",
     overridingMethod: "POST"
-  });
-});
-
-router.get('/form/:id', function (req, res, next) {
-  var productId = req.params.id;
-
-  Product.findById(productId, function (err, product) {
-
-    res.render('product/product-form', {
-      heading: "Edit Product",
-      url: "/product/edit/" + productId,
-      overridingMethod: "PATCH",
-      title: product.title,
-      description: product.description,
-      price: product.price,
-    });
   });
 });
 
@@ -60,6 +51,23 @@ router.post('/add', function (req, res, next) {
       res.redirect('/');
     });
 
+  });
+});
+
+// Edit product
+router.get('/form/:id', function (req, res, next) {
+  var productId = req.params.id;
+
+  Product.findById(productId, function (err, product) {
+
+    res.render('product/product-form', {
+      heading: "Edit Product",
+      url: "/product/edit/" + productId,
+      overridingMethod: "PATCH",
+      title: product.title,
+      description: product.description,
+      price: product.price,
+    });
   });
 });
 
@@ -88,9 +96,24 @@ router.post('/edit/:id', function (req, res, next) {
         });
       });
     }
-
   });
-
 });
 
+router.get('/delete/:id', function (req, res, next) {
+  var productId = req.params.id;
+  Product.remove({_id: productId}, function (err, product) {
+    if (err) return res.write(err);
+    res.redirect('/');
+  });
+});
+
+
 module.exports = router;
+
+
+function isLoggedIn(req, res, next) {
+  if(req.isAuthenticated())
+    return next();
+  res.redirect('/user/signin');
+}
+
