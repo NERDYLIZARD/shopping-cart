ff7e5e9260b5efa36816ab725f694d97533201f6
NERDYLIZARD <bouasavanhhop@gmail.com>
2017-07-08 12:29:36 +0530

    upload image with multer

--- routes/product.js
+++ routes/product.js
@@ -2,15 +2,17 @@
  * Created by Hoppies on 09-Mar-17.
  */
 
+var csrf = require('csurf');
 var express = require('express');
-var router = express.Router();
-var fs = require('fs');
+var multer = require('multer');
 var passport = require('passport');
-var csrf = require('csurf')();
 
 var Product = require('../models/product');
 
-router.use(csrf);
+var upload = multer();
+var router = express.Router();
+var csrfProtection = csrf();
+router.use(csrfProtection);
 
 router.get('/:id/image', function (req, res, next) {
   Product.findById(req.params.id, function (err, product) {
@@ -37,28 +39,26 @@ router.get('/form', function (req, res, next) {
 });
 
 
-router.post('/add', function (req, res, next) {
+router.post('/add', upload.single('productImage'), function (req, res, next) {
 
-  var product = new Product();
-
-  var path = "C:/Users/Hoppies/Desktop/";
-  fs.readFile(path + req.body.productImage, function (err, data) {
-    product.image.data = data;
-    // get extension
-    product.image.contentType = req.body.productImage.split('.').pop();
-    product.title =  req.body.title;
-    product.description = req.body.description;
-    product.price = +req.body.price;
-    product.seller = req.user;
-
-    var previousUrl = req.session.previousUrl;
-    product.save(function (err, product) {
-      if(err) throw err;
-      req.session.previousUrl = null;
-      res.redirect(previousUrl);
-    });
-
+  var product = new Product({
+    title:  req.body.title,
+    description: req.body.description,
+    price: +req.body.price,
+    seller: req.user,
+    image: {
+      data: req.file.buffer,
+      contentType: req.file.mimetype.split('/').pop()
+    },
   });
+
+  var previousUrl = req.session.previousUrl;
+  product.save(function (err, product) {
+    if(err) throw err;
+    req.session.previousUrl = null;
+    res.redirect(previousUrl);
+  });
+
 });
 
 // Edit product
@@ -71,7 +71,6 @@ router.get('/form/:id', function (req, res, next) {
       return res.redirect('/');
     }
     req.session.previousUrl = req.headers.referer;
-    console.log(product.title);
     res.render('product/product-form', {
       heading: "Edit Product",
       url: "/product/edit/" + productId,
@@ -85,7 +84,7 @@ router.get('/form/:id', function (req, res, next) {
 });
 
 
-router.post('/edit/:id', function (req, res, next) {
+router.post('/edit/:id', upload.single('productImage'), function (req, res, next) {
 
   var productId = req.params.id;
   Product.findById(productId, function (err, product) {
@@ -99,23 +98,18 @@ router.post('/edit/:id', function (req, res, next) {
     product.price = +req.body.price;
 
     var previousUrl = req.session.previousUrl;
-    if (!req.body.productImage) {
+    if (!req.file) {
       product.save(function (err, product) {
         if (err) throw err;
         res.redirect(previousUrl);
-
       });
     } else {
-      var path = "C:/Users/Hoppies/Desktop/";
-      fs.readFile(path + req.body.productImage, function (err, data) {
-        console.log(data);
-        product.image.data = data;
-        product.image.contentType = req.body.productImage.split('.').pop();
+      product.image.data = req.file.buffer;
+      product.image.contentType = req.file.mimetype.split('/').pop();
 
-        product.save(function (err, product) {
-          if (err) throw err;
-          res.redirect(previousUrl);
-        });
+      product.save(function (err, product) {
+        if (err) throw err;
+        res.redirect(previousUrl);
       });
     }
     req.session.previousUrl = null;
